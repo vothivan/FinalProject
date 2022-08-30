0c9aa98bd55472d33cfccb89c1520ff7c5bec974
unknown <van.meo.0409@gmail.com>
2022-08-31 01:34:51 +0700

    mockup

--- src/view/learn/index.jsx
+++ src/view/learn/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Container, Tab, Card, Grid, Button, Menu, MenuItem } from '@material-ui/core';
+import { Avatar, Container, Tab, Card, Grid, Button, Menu, MenuItem } from '@material-ui/core';
 import { TabContext, TabList, TabPanel } from '@material-ui/lab';
 
 
@@ -18,22 +18,28 @@ export default function Test() {
         setAnchorEl(null);
     };
     return (
-        <Card style={{ alignItem: 'center', width: '800px', marginLeft: 'auto', marginRight: 'auto', height: '800px' }}>
+        <Grid>
             <Container>
-                <Box>
-                    <TabContext value={value}>
+                <TabContext value={value}>
+                    <Card>
                         <Grid >
                             <TabList alignItems='center' onChange={handleChange}
                                 textColor="primary"
                                 variant="fullWidth"
                                 aria-label="full width tabs example">
-                                <Tab lg={3} label="Learn" value="learn">
-                                </Tab>
-                                <Tab lg={3} label="Shop" value="shop" />
-                                <Tab lg={3} label="School" value="school" />
-                                <div alignItem='center' >
+                                <Button lg={3}><Tab label={<img src='https://d35aaqx5ub95lt.cloudfront.net/vendor/784035717e2ff1d448c0f6cc4efc89fb.svg' />} value="learn"/>
+                                    <text>Học</text>
+                                </Button>
+                                <Button lg={3}><Tab label={<img src='https://d35aaqx5ub95lt.cloudfront.net/vendor/0e58a94dda219766d98c7796b910beee.svg' />} value="shop" />
+                                    <text>Cửa hàng</text>
+                                </Button>
+                                <Button lg={3}><Tab label={<img src='https://d35aaqx5ub95lt.cloudfront.net/vendor/7159c0b5d4250a5aea4f396d53f17f0c.svg' />} value="school" />
+                                    <text>Xem thêm</text>
+                                </Button>
+                                <div lg={3}>
                                     <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                                        Open Menu
+                                        <Avatar alt="Remy Sharp" src="https://simg-ssl.duolingo.com/avatars/989534973/O_Iq9fnCY-/medium" />
+                                        {/* <img src='https://simg-ssl.duolingo.com/avatars/989534973/O_Iq9fnCY-/medium' alt="Avatar" /> */}
                                     </Button>
                                     <Menu
                                         id="simple-menu"
@@ -50,19 +56,19 @@ export default function Test() {
                             </TabList>
 
                         </Grid>
-                        <TabPanel value="learn">
-                            <img alt="cropped" src={'https://d35aaqx5ub95lt.cloudfront.net/vendor/784035717e2ff1d448c0f6cc4efc89fb.svg'} />
-                        </TabPanel>
-                        <TabPanel value="shop">
-                            <img alt="cropped" src={'https://d35aaqx5ub95lt.cloudfront.net/vendor/784035717e2ff1d448c0f6cc4efc89fb.svg'} />
-                        </TabPanel>
-                        <TabPanel value="school">
-                            <img alt="cropped" src={'https://d35aaqx5ub95lt.cloudfront.net/vendor/784035717e2ff1d448c0f6cc4efc89fb.svg'} />
-                        </TabPanel>
-                    </TabContext>
-                </Box>
+                    </Card>
+                    <TabPanel value="learn">
+                        <img alt="cropped" src={'https://d35aaqx5ub95lt.cloudfront.net/vendor/784035717e2ff1d448c0f6cc4efc89fb.svg'} />
+                    </TabPanel>
+                    <TabPanel value="shop">
+                        <img alt="cropped" src={'https://d35aaqx5ub95lt.cloudfront.net/vendor/784035717e2ff1d448c0f6cc4efc89fb.svg'} />
+                    </TabPanel>
+                    <TabPanel value="school">
+                        <img alt="cropped" src={'https://d35aaqx5ub95lt.cloudfront.net/vendor/784035717e2ff1d448c0f6cc4efc89fb.svg'} />
+                    </TabPanel>
+                </TabContext>
             </Container>
-        </Card >
+        </Grid>
 
     );
 }
