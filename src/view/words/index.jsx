bc84fb5f0a12611deb5b4ad68de30f30a75fdc7b
vothivan <van.meo.0409@gmail.com>
2022-10-11 23:41:24 +0700

    revert code

--- src/view/words/index.jsx
+++ src/view/words/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './style.css';
-import ArrowForwardIosSharpIcon from '@material-ui/icons/ArrowForwardIosSharp';
 const Data = {
     'Du Lịch': ['Tại sân bay', 'Tại khách sạn', 'Tại nhà hàng', 'Tại bảo tàng'],
     'Thức ăn': ['Trái cây', 'Rau củ', 'Thức ăn nhanh', 'Thức uống'],
@@ -12,7 +11,7 @@ function Words() {
             <h1 className='title'>Words</h1>
             <header className='header'>
                 <span className='header-left'>Du Lich</span>
-                <button className='header-right'>Xem tat ca <ArrowForwardIosSharpIcon style={{fontSize: '15px'}}></ArrowForwardIosSharpIcon></button>
+                <button className='header-right'>Xem tat ca</button>
             </header>
             {/* <div className='list-vocabulary'>
                 {for (key in Data) {
