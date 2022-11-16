3a67411afdb76ade70a26c96150ab94f4e66ca35
hoang.tranquang <hoang.tranquang@vti.com.vn>
2022-11-16 10:47:05 +0700

    add guide grammar

---
+++ src/view/grammar/guide.jsx
@@ -0,0 +1,99 @@
+import { Paper, TableCell, TableContainer, Table, TableBody, TableRow } from '@material-ui/core';
+// import { Table, TableBody } from 'material-ui';
+import React from 'react';
+import './style.css'
+
+function toStyle(obj = {}) {
+    const style = {};
+    for (const [key, value] of Object.entries(obj)) {
+        if (key !== 'text') {
+            style['--' + key] = value;
+
+        }
+    }
+    return style;
+}
+
+function Guide({ guide = {} }) {
+    const { items=[], title, header } = guide;
+    return (
+        <div>
+            <h2>{title}</h2>
+            <h1>{header}</h1>
+            {
+                items.map((item, index) => <GuideItem key={index} item={item} />)
+            }
+        </div>
+
+    )
+}
+
+function GuideItem({ item }) {
+    switch (item.type) {
+        case "text":
+            return (<p>
+                <GuideItemContent contents={item.content} />
+            </p>)
+
+        case "table_two_columns":
+            return (
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableBody>
+                            {
+                                item.content.map((content, idx) => (
+                                    <TableRow key={idx} >
+                                        <TableCell>
+                                            <GuideItemContent contents={content.text} />
+                                        </TableCell>
+                                        <TableCell>
+                                            <GuideItemContent contents={content.value} />
+                                        </TableCell>
+                                    </TableRow>
+                                ))
+                            }
+                        </TableBody>
+                    </Table>
+
+                </TableContainer>
+            )
+        case "table_one_column":
+            return (
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableBody>
+                            {
+                                item.content.map((content, idx) => (
+                                    <TableRow key={idx} >
+                                        <TableCell>
+                                            <GuideItemContent contents={content.text} />
+                                            <br />
+                                            <GuideItemContent contents={content.value} />
+                                        </TableCell>
+                                    </TableRow>
+                                ))
+                            }
+                        </TableBody>
+                    </Table>
+
+                </TableContainer>
+            )
+        default:
+            return (
+                <div>
+                    {item.type}
+                </div>
+            )
+    }
+
+}
+
+function GuideItemContent({ contents = [] }) {
+    return (
+        contents.map((content, idx) => (
+            <span className={'txt_guide'} key={idx} style={toStyle(content)}>{content.text || ' '}</span>
+        ))
+    );
+}
+
+export default Guide;
--- src/view/grammar/lesson_all.jsx
+++ src/view/grammar/lesson_all.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
-import {Button } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import { redirectRouter } from '../../utils/common';
 import { ArrowBack } from '@material-ui/icons';
 import './style.css';
 import api from '../../service/api';
+import Guide from './guide';
 class LessonAll extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             grammar: [],
+            grammarDetail: {}
         }
     }
     componentDidMount() {
-        const {id} = this.props.match.params;
+        const { id } = this.props.match.params;
         api.get("/grammar", id).then((res) => {
-            if (res && res.status === 200){
+            if (res && res.status === 200) {
                 this.setState({
                     grammar: res.data,
+                    grammarDetail: res.data.find(g => g.id === +id)
                 })
             }
         })
@@ -27,18 +30,20 @@ class LessonAll extends Component {
      * @returns 
      */
     render() {
+        const { grammarDetail } = this.state;
         return (
-            <div style={{height: '100%'}}>
+            <div style={{ height: '100%' }}>
                 <div style={{ display: 'flex' }}>
-                    <div style={{display: 'flex', alignItems: 'flex-start'}}>
+                    <div style={{ display: 'flex', alignItems: 'flex-start' }}>
                         <Button onClick={() => redirectRouter(this.props, '/learn/grammar')}><ArrowBack /></Button>
                     </div>
-                    <div alignItems='center' justify='flex-end'>
+                    <div style={{ alignItems: 'center', justify: 'flex-end' }}>
+                        <Guide guide={grammarDetail.descriptions} />
                     </div>
                 </div>
-                <div className='total' style={{display: 'flex', alignItems: 'flex-end'}}>
+                <div className='total' style={{ display: 'flex', alignItems: 'flex-end' }}>
                     <div className='button'>
-                        <div style={{ width: '100%', margin: '16px 0px 0px' }}><Button className='button-child' style={{width: '100%', alignItems: 'end', borderRadius: '20px'}} onClick={() => redirectRouter(this.props, '/login')}>Learn</Button></div>
+                        <div style={{ width: '100%', margin: '16px 0px 0px' }}><Button className='button-child' style={{ width: '100%', alignItems: 'end', borderRadius: '20px' }} onClick={() => redirectRouter(this.props, '/login')}>Learn</Button></div>
                     </div>
                 </div>
             </div>
