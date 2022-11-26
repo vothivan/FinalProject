ca104cb18637d9e6a701d0412fad12815e90a8b3
hoang tran <hoangtq1607@gmail.com>
2022-11-26 17:41:23 +0700

    add grammar learn

--- src/view/grammar/learn.jsx
+++ src/view/grammar/learn.jsx
@@ -1,63 +1,174 @@
-import React, { Component } from "react";
-import { Button } from "@material-ui/core";
+import React, { Component, useEffect, useState } from "react";
+import { Button, Chip } from "@material-ui/core";
 import CheckIcon from '@material-ui/icons/Check';
 import ClearIcon from '@material-ui/icons/Clear';
 import CancelIcon from '@material-ui/icons/Cancel';
-import { redirectRouter } from "../../utils/common";
-class LearnGrammar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            status: 'question',
-            check: false,
-            answer: '___'
+import api from '../../service/api';
+import { Link, useParams } from "react-router-dom";
+import LoadingOverlay from 'react-loading-overlay';
+LoadingOverlay.propTypes = undefined
+
+const btnStyle = { fontWeight: 'bold', fontSize: '17px', textTransform: 'none', height: '50px', borderRadius: '20px', marginBottom: '10px', width: '100%', boxShadow: 'rgb(0 0 0 / 15%) 0px 4px 32px', textAlign: 'center', alignItems: 'center' };
+
+const correctAudio = new Audio("/audio/correct-6033.mp3");
+const errorAudio = new Audio("/audio/windows-error-sound-effect.mp3");
+
+export default function LearnGrammar(props) {
+
+    const { id } = useParams();
+
+    const [questions, setQuestions] = useState([]);
+
+    const [userChoose, setUserChoose] = useState(new Set());
+
+    const [checking, setChecking] = useState(false);
+
+    const [isCorrect, setIsCorrect] = useState(false);
+
+    const [questionState, setQuestionState] = useState('start');
+
+    const [serverAnswer, setServerAnswer] = useState({});
+
+    const [questionIndex, setQuestionIndex] = useState(0);
+
+    const question = { ...(questions[questionIndex] || {}) };
+
+    const splitQuestion = (question.question || '').split(/{.+}/)
+
+    useEffect(() => {
+        api.get('/grammar/' + id)
+            .then(res => setQuestions(res.data.questions))
+    }, [])
+
+    const onCheckAnswer = async (chooses) => {
+
+        setChecking(true)
+        const payload = {
+            grammarQuestionId: question.id,
+            answers: chooses
+        }
+        try {
+            const { data } = await api.post("/grammar/check", payload);
+            setIsCorrect(data.correct)
+            setServerAnswer(data);
+            if (data.correct) {
+                correctAudio.play();
+            } else {
+                errorAudio.play();
+            }
+        } finally {
+            setQuestionState('end')
+            setChecking(false)
         }
     }
-    checkQuestion = (answer) => {
-        this.setState({
-            status: 'answer',
-        })
-        if (answer === true) {
-            this.setState({
-                check: true,
-            })
-        } else this.setState({
-            check: false,
-        })
-    }
-    render() {
-        return (
-            <div className="root">
-                <Button onClick={() => redirectRouter(this.props, '/learn/word/list-word/' + this.props.match.params.id)}>
-                    <CancelIcon />
-                </Button>
 
-                <div style={{ margin: '30px auto 30px auto', textAlign: 'center', fontWeight: '600', color: '#a1a1a1' }}>Choose the correct anwser!</div>
-                <h3 style={{textAlign: 'center'}}>Tôi dậy sớm.</h3>
-                <h4 style={{textAlign: 'center'}}>I {this.state.answer } early.</h4>
-                <div>
-                    <Button label="Get up"  onClick={() => this.checkQuestion(true)} style={{ fontWeight: 'bold', fontSize: '17px', textTransform: 'none', height: '50px', borderRadius: '20px', marginBottom: '10px', width: '100%', boxShadow: 'rgb(0 0 0 / 15%) 0px 4px 32px', textAlign: 'center', alignItems: 'center' }}>
-                    </Button>
-                    <Button onClick={() => this.checkQuestion(false)} style={{ fontWeight: 'bold', fontSize: '17px', textTransform: 'none', height: '50px', borderRadius: '20px', marginBottom: '10px', width: '100%', boxShadow: 'rgb(0 0 0 / 15%) 0px 4px 32px', textAlign: 'center', alignItems: 'center' }}>
-                        Gets up
-                    </Button>
-                    <Button onClick={() => this.checkQuestion(false)} style={{ fontWeight: 'bold', fontSize: '17px', textTransform: 'none', height: '50px', borderRadius: '20px', marginBottom: '10px', width: '100%', boxShadow: 'rgb(0 0 0 / 15%) 0px 4px 32px', textAlign: 'center', alignItems: 'center' }}>
-                        Gotten
-                    </Button>
-                </div>
-                <div style={{ textAlign: 'center' }}>
-                    {this.state.check === true ?
-                        <div style={{ display: 'block' }}>
-                            <CheckIcon style={{ color: 'green', fontSize: '40px' }} />
-                            <div>Correct!</div>
-                        </div> :
-                        <div style={{ display: 'block' }}>
-                            <ClearIcon style={{ color: 'red', fontSize: '40px' }} />
-                            <div>Incorrect!</div>
-                        </div>}
-                </div>
-            </div>
-        )
+    const checkAnswer = async (choose) => {
+        setUserChoose([choose])
+        onCheckAnswer([choose])
     }
+
+    const addChoose = choose => {
+        setUserChoose(old => new Set([...old, choose]))
+    }
+
+    const onNextQuestion = () => {
+        setQuestionState('start')
+        setServerAnswer({})
+        setUserChoose([])
+        setQuestionIndex(id => id + 1)
+    }
+
+    return (
+        <div className="root">
+
+            <Link to={'/learn/grammar/lesson_all/' + id}>
+                <CancelIcon />
+            </Link>
+            <div>
+                <progress value={questionIndex + 1} max={questions.length}></progress>
+                {questionIndex + 1}/{questions.length} questions
+            </div>
+            <div style={{ margin: '30px auto 30px auto', textAlign: 'center', fontWeight: '600', color: '#a1a1a1' }}>{question.questionNative}</div>
+            <h3 style={{ textAlign: 'center' }}>{question.contentNative}</h3>
+
+            {/* ---------------------CHOOSE-------------------- */}
+            {
+                (question.type === 'WRITE') && <div>
+                    <h4 style={{ textAlign: 'center' }}>
+                        {splitQuestion[0]}
+                        <Chip size="small" label={userChoose} variant="outlined" style={{ minWidth: 40 }} />
+                        {splitQuestion[1]}
+                    </h4>
+                    {
+                        (questionState === 'start') && (
+                            <LoadingOverlay active={checking} spinner>
+                                {question.choose?.map(choose => (
+                                    <Button key={choose} style={btnStyle} onClick={() => checkAnswer(choose)} >
+                                        {choose}
+                                    </Button>
+                                ))}
+                            </LoadingOverlay>
+                        )
+                    }
+                </div>
+
+            }
+
+            {/* ---------------------CHOOSE-------------------- */}
+            {
+                (question.type === 'SORT' && questionState === 'start') && (
+                    <div>
+                        <div>
+                            Answer: {[...userChoose].map(a => (
+                                <Button size="small" variant="outlined" key={a}
+                                    onClick={() => setUserChoose(old => {
+                                        old.delete(a);
+                                        return new Set(old);
+                                    })}
+                                >
+                                    {a}
+                                </Button>
+                            ))}
+                        </div>
+                        <LoadingOverlay active={checking} spinner>
+                            <div>
+                                {question.choose?.map(choose => (
+                                    <Button variant="outlined" key={choose} onClick={() => addChoose(choose)} >
+                                        {choose}
+                                    </Button>
+                                ))}
+                            </div>
+                            <div>
+                                <Button variant="outlined" disabled={userChoose.size == 0} onClick={() => onCheckAnswer([...userChoose])} >
+                                    Submit
+                                </Button>
+                            </div>
+                        </LoadingOverlay>
+                    </div>
+                )
+
+            }
+
+            {
+                (questionState == 'end') &&
+                <div style={{ textAlign: 'center' }}>
+                    <div style={{ display: 'block' }}>
+                        {isCorrect ? (
+                            <div>
+                                <CheckIcon style={{ color: 'green', fontSize: '40px' }} />
+                                <div>Correct!</div>
+                            </div>
+                        ) : (
+                            <div>
+                                <ClearIcon style={{ color: 'red', fontSize: '40px' }} />
+                                <div>Incorrect!</div>
+                                <div>Correct answer: {serverAnswer.correctAnswers}</div>
+                            </div>
+                        )}
+                    </div>
+                    <Button variant="outlined" onClick={onNextQuestion}>Next</Button>
+                </div>
+            }
+        </div>
+    )
 }
-export default LearnGrammar;
--- src/view/grammar/lesson_all.jsx
+++ src/view/grammar/lesson_all.jsx
@@ -1,53 +1,44 @@
-import React, { Component } from 'react';
-import { Button } from '@material-ui/core';
+import React, { Component, useEffect, useState } from 'react';
+import { Button, Tooltip } from '@material-ui/core';
 import { redirectRouter } from '../../utils/common';
 import { ArrowBack } from '@material-ui/icons';
 import './style.css';
 import api from '../../service/api';
 import Guide from './guide';
-class LessonAll extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            grammar: [],
-            grammarDetail: {}
-        }
-    }
-    componentDidMount() {
-        const { id } = this.props.match.params;
-        api.get("/grammar", id).then((res) => {
+import { Link } from 'react-router-dom';
+
+export default function LessonAll(props) {
+    const { id } = props.match.params;
+    const [grammar, setGrammar] = useState({});
+
+    useEffect(() => {
+        api.get("/grammar/" + id).then((res) => {
             if (res && res.status === 200) {
-                this.setState({
-                    grammar: res.data,
-                    grammarDetail: res.data.find(g => g.id === +id)
-                })
+                setGrammar(res.data)
             }
         })
-    }
+    }, [])
 
-    /**
-     * render
-     * @returns 
-     */
-    render() {
-        const { grammarDetail } = this.state;
-        return (
-            <div style={{ height: '100%' }}>
-                <div>
-                    <div style={{ display: 'flex', alignItems: 'flex-start' }}>
-                        <Button onClick={() => redirectRouter(this.props, '/learn/grammar')}><ArrowBack /></Button>
-                    </div>
-                    <div style={{ alignItems: 'center', margin: '0px 16px 0px 16px' }}>
-                        <Guide guide={grammarDetail.descriptions} />
-                    </div>
+    return (
+        <div style={{ height: '100%' }}>
+            <div>
+                <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+                    <Button onClick={() => redirectRouter(props, '/learn/grammar')}><ArrowBack /></Button>
                 </div>
-                <div className='total' style={{ display: 'flex', alignItems: 'flex-end' }}>
-                    <div className='button'>
-                        <div style={{ width: '100%', margin: '16px 0px 0px' }}><Button className='button-child' style={{fontSize: '17px', width: '100%', alignItems: 'center', borderRadius: '20px', textTransform: 'none', fontWeight: 'bold' }} onClick={() => redirectRouter(this.props, '/learn-grammar/' + this.props.match.params.id)}>Learn</Button></div>
+                <div style={{ alignItems: 'center', margin: '0px 16px 0px 16px' }}>
+                    <Guide guide={grammar?.descriptions} />
+                </div>
+            </div>
+            <div className='total' style={{ display: 'flex', alignItems: 'flex-end' }}>
+                <div className='button'>
+                    <div style={{ width: '100%', margin: '16px 0px 0px' }}>
+                        <Button className='button-child' disabled={grammar.questions?.length === 0}
+                            style={{ fontSize: '17px', width: '100%', alignItems: 'center', borderRadius: '20px', textTransform: 'none', fontWeight: 'bold' }}
+                            onClick={() => redirectRouter(props, '/learn-grammar/' + props.match.params.id)}>Learn {grammar.questions?.length === 0 ? "(no any questions)" : ""}</Button>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+
 }
-export default LessonAll;
